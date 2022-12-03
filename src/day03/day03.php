3fc462c3b25225abfe794e960b58a0fb2ef2c356
Joshua Nye <joshua.nye@gmail.com>
2022-12-03 10:51:17 -0500

    Minor tweaks

--- src/day03/day03.php
+++ src/day03/day03.php
@@ -6,17 +6,19 @@ use Illuminate\Support\Collection;
 
 $answer1 = (new Collection(file('input.txt', FILE_IGNORE_NEW_LINES)))
     ->map(fn($line) => (new Collection(str_split($line)))->split(2))
-    ->map(fn($sacks) => $sacks[0]->intersect($sacks[1])->unique()->first())
-    ->map(fn($item) => ord($item) > 90 ? ord($item) - ord('a') + 1 : ord($item) - ord('A') + 27)
+    ->map(fn($sacks) => $sacks[0]->intersect($sacks[1])->first())
+    ->map(fn($item) => ord($item))
+    ->map(fn($value) => $value > 90 ? $value - ord('a') + 1 : $value - ord('A') + 27)
     ->sum();
 
 print "Answer 1: {$answer1}\n";
 
 $answer2 = (new Collection(file('input.txt', FILE_IGNORE_NEW_LINES)))
-    ->map(fn($line) => (new Collection(str_split($line))))
+    ->map(fn($line) => new Collection(str_split($line)))
     ->chunk(3)
-    ->map(fn($group) => $group->reduce(fn($carry, $item) => $carry == null ? $item : $carry->intersect($item))->unique()->first())
-    ->map(fn($item) => ord($item) > 90 ? ord($item) - ord('a') + 1 : ord($item) - ord('A') + 27)
+    ->map(fn($group) => $group->reduce(fn($carry, $item) => $carry === null ? $item : $carry->intersect($item))->first())
+    ->map(fn($item) => ord($item))
+    ->map(fn($value) => $value > 90 ? $value - ord('a') + 1 : $value - ord('A') + 27)
     ->sum();
 
 print "Answer 2: {$answer2}\n";
