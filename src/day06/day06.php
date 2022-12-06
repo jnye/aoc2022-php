376ff42d36b7608704c85dd85c65c038ce30310b
Joshua Nye <joshua.nye@gmail.com>
2022-12-06 08:10:31 -0500

    Day 6 part 2 solution

--- src/day06/day06.php
+++ src/day06/day06.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../common.php';
 use Illuminate\Support\Collection;
 
 $input = str_split(file('input.txt', FILE_IGNORE_NEW_LINES)[0]);
+
 $answer1 = 0;
 for ($i = 0; $i < count($input); $i++) {
     $slice = new Collection(array_slice($input, 0, $i));
@@ -16,3 +17,15 @@ for ($i = 0; $i < count($input); $i++) {
 }
 
 print "Answer 1: {$answer1}\n";
+
+$answer2 = 0;
+for ($i = 0; $i < count($input); $i++) {
+    $slice = new Collection(array_slice($input, 0, $i));
+    $count = $slice->pop(14)->unique()->count();
+    if ($count == 14) {
+        $answer2 = $i;
+        break;
+    }
+}
+
+print "Answer 2: {$answer2}\n";
