e98f01c94b1575b90784de102dd62c8970d2978b
Joshua Nye <joshua.nye@gmail.com>
2022-12-06 09:10:45 -0500

    Day 6 refactor to standard PHP functions

--- src/day06/day06.php
+++ src/day06/day06.php
@@ -2,30 +2,21 @@
 
 require_once __DIR__ . '/../common.php';
 
-use Illuminate\Support\Collection;
-
 $input = str_split(file('input.txt', FILE_IGNORE_NEW_LINES)[0]);
 
-$answer1 = 0;
-for ($i = 0; $i < count($input); $i++) {
-    $slice = new Collection(array_slice($input, 0, $i));
-    $count = $slice->pop(4)->unique()->count();
-    if ($count == 4) {
-        $answer1 = $i;
-        break;
+function find_start(array $input, int $len): int
+{
+    for ($i = 0; $i < count($input) - $len; $i++) {
+        $count = count(array_unique(array_slice($input, $i, $len)));
+        if ($count == $len) {
+            return $i + $len;
+        }
     }
+    return -1;
 }
 
+$answer1 = find_start($input, 4);
 print "Answer 1: {$answer1}\n";
 
-$answer2 = 0;
-for ($i = 0; $i < count($input); $i++) {
-    $slice = new Collection(array_slice($input, 0, $i));
-    $count = $slice->pop(14)->unique()->count();
-    if ($count == 14) {
-        $answer2 = $i;
-        break;
-    }
-}
-
+$answer2 = find_start($input, 14);
 print "Answer 2: {$answer2}\n";
