6962bec54af7c1af6b71efd89152451157f8b23f
Joshua Nye <joshua.nye@gmail.com>
2022-12-11 11:11:52 -0500

    Day 10 solution 1 rework

--- src/day10/day10.php
+++ src/day10/day10.php
@@ -5,28 +5,32 @@ use Webmozart\Assert\Assert;
 require_once __DIR__ . '/../common.php';
 
 $instructions = file('input.txt', FILE_IGNORE_NEW_LINES);
-$reg = 1;
-$reg_next = $reg;
+$x = 1;
 $cycle = 0;
-$trace = [];
-$pipeline = [];
+$cycles_till_complete = 0;
+$next_x = $x;
+$trace = []; // value of x during a cycle
 foreach ($instructions as $instruction) {
     $parts = explode(' ', $instruction);
     if ($parts[0] == 'noop') {
-        $trace[$cycle++] = $reg;
+        $cycles_till_complete = 1;
+        $next_x = $x;
     } else if ($parts[0] == 'addx') {
-        $value = intval($parts[1]);
-        $trace[$cycle++] = $reg;
-        $trace[$cycle++] = $reg + $value;
-        $reg = $reg + $value;
+        $cycles_till_complete = 2;
+        $next_x = $x + intval($parts[1]);
     } else throw new InvalidArgumentException($instruction);
+    while ($cycles_till_complete > 0) {
+        $cycles_till_complete--;
+        $cycle++;
+        $trace[$cycle] = $x;
+    }
+    $x = $next_x;
 }
 
 $sum = 0;
 for ($cycle = 20; $cycle < count($trace); $cycle += 40) {
-    $i = $cycle - 2;
-    $reg = $trace[$i];
-    $sig = $cycle * $reg;
+    $x = $trace[$cycle];
+    $sig = $cycle * $x;
     $sum += $sig;
 }
 $answer1 = $sum;
